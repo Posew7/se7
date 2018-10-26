48866e19c36a2d967f7e457ce15ae1d0afa41cd5
posew7 <posew7@gmail.com>
2018-10-26 22:26:23 +0300

    fonksiyon örnek 2

---
+++ src/fonk2.java
@@ -0,0 +1,16 @@
+
+public class fonk2 {
+
+    public static void selamla(String isim) {
+        System.out.println("selamlar " + isim);
+    }
+
+    public static void topla(int a, int b, int c) {
+        System.out.println("sayıların toplamı :" + (a + b + c));
+    }
+
+    public static void main(String[] args) {
+        selamla("Posew7");
+        topla(7, 11, 13);
+    }
+}
