f676a07cb21920eef7a39ad886252c760a431fe9
posew7 <posew7@gmail.com>
2018-10-29 09:10:00 +0300

    string bazı özellikleri

---
+++ src/stringozellik.java
@@ -0,0 +1,21 @@
+
+public class stringozellik {
+
+    public static void main(String[] args) {
+        
+        String a = "Posew7";
+        String b = "Posew7";
+        
+        System.out.println("0.index : "+a.charAt(0));
+        System.out.println();
+        System.out.println("5.index : "+a.charAt(5));
+        System.out.println();
+        System.out.println("e harfinin indexi : "+a.indexOf("e"));
+        System.out.println();
+        System.out.println(a.toUpperCase());
+        System.out.println();
+        System.out.println(a.toLowerCase());
+        System.out.println();
+        
+    }
+}
